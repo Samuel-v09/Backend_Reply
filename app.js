f3d590874bce4b763f92f6e1264e8454823825b3
Samuel <samuellwat978@gmail.com>
2024-10-09 18:41:02 -0300

    Alteração Yuri Backend

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const pool = require('./config/db');
-const g_routes = require('./routes_general/g_routes');
-const middleware = require('./middleware_auth');
-const cors = require('cors');
+const pool = require("./config/db");
+const g_routes = require("./routes_general/g_routes");
+const middleware = require("./middleware_auth");
+const cors = require("cors");
 
-app.use(cors({
-    origin: 'https://frontend-reply.onrender.com'
-}));
+app.use(
+  cors({
+    origin: "https://frontend-emtq.onrender.com",
+  })
+);
 
 app.use(express.json());
 
-app.use('/', g_routes);
-
-app.use('/inserir_dados_motores', middleware);
-app.use('/colentando_dados_motores', middleware);
+app.use("/", g_routes);
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Algo deu errado capturado pelo middleware!');
+  console.error(err.stack);
+  res.status(500).send("Algo deu errado capturado pelo middleware!");
 });
 
-process.on('SIGTERM', () => {
-    pool.end(() => {
-        console.log('Pool de Conexões fechado Termino do processo');
-        process.exit(0);
-    });
+process.on("SIGTERM", () => {
+  pool.end(() => {
+    console.log("Pool de Conexões fechado Termino do processo");
+    process.exit(0);
+  });
 });
 
-process.on('SIGINT', () => {
-    pool.end(() => {
-        console.log('Pool de Conexões fechado Termino do processo efetuado pelo terminal');
-        process.exit(0);
-    });
+process.on("SIGINT", () => {
+  pool.end(() => {
+    console.log(
+      "Pool de Conexões fechado Termino do processo efetuado pelo terminal"
+    );
+    process.exit(0);
+  });
 });
 module.exports = app;
